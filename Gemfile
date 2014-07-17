d3ee7ba3b935df928b2080071f7727e971e02803
Paul Navasard <paul.navasard@domo.com>
2014-07-16 22:04:36 -0600

    Added Sass

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'puma'
+gem 'sass'
 
 group :development do
   gem 'guard'
--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 class Aldente < Sinatra::Base
 
   get '/' do
+    content_type :html
     send_file 'index.html'
+  end
+
+  get '/styles/*.css' do |sheet_name|
+    content_type :css
+    scss :"scss/#{sheet_name}"
   end
 
 end
