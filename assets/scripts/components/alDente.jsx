9f50d7174f358ae7ea6b00d1c3b4b36783a4e071
Paul Navasard <paul.navasard@domo.com>
2014-08-19 23:05:47 -0600

    Better details page

--- assets/scripts/components/alDente.jsx
+++ assets/scripts/components/alDente.jsx
@@ -1,12 +1,21 @@
 /** @jsx React.DOM */
 var React = require('../vendor/react/react.js');
 var Header = require('./header.jsx');
+var RecipeDetail = require('./recipeDetail.jsx');
+
+var recipe = {
+  name: 'German Pankackes',
+  image_url: 'http://www.creative-culinary.com/wp-content/uploads/germanpancake-serving.jpg',
+  prep_time: '20',
+  tags: [ 'Breakfast', 'Dinner', '9X13' ]
+}
 
 var alDente = React.createClass({
   render: function() {
     return (
-      <div className="app">
-        <Header user={this.props.user}></Header>
+      <div id="csstyle">
+        <Header user={this.props.user} />
+        <RecipeDetail recipe={recipe} />
       </div>
     );
   }
